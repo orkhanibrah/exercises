39ea663056414d87487b0ba4e98a8a81de5c56d8
orkhan <orkhan.ibrah@gmail.com>
2023-07-30 01:25:32 +0300

    day 1 final commit

--- Week3_Day1/exercisesXP.js
+++ Week3_Day1/exercisesXP.js
@@ -31,4 +31,89 @@ console.log(myWatchedSeries);
 
 
 //Exercise 3
+let celcius = 0;
+console.log(`${celcius}°C is ${celcius/5*9+32}°F`);
 
+
+//Exercise 4
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a+b) //first expression
+// Prediction: 55 because both are numbers
+// Actual: 55
+
+a = 2;
+
+console.log(a+b) //second expression
+// Prediction: 23 because a is changed to 2.
+// Actual: 23
+
+//c is undefined because there's no value yet.
+
+
+//Exercise 5
+console.log(typeof(15))
+// Prediction: number
+// Actual: number
+
+console.log(typeof(5.5))
+// Prediction: number
+// Actual: number
+
+console.log(typeof(NaN))
+// Prediction: boolean because it returns true or false
+// Actual: number
+
+console.log(typeof("hello"))
+// Prediction: hello is a string
+// Actual: string
+
+console.log(typeof(true))
+// Prediction: boolean
+// Actual: boolean
+
+console.log(typeof(1 != 2))
+// Prediction: boolean because 1!= 2 is true
+// Actual: boolean
+
+console.log("hamburger" + "s")
+// Prediction: hamburgers
+// Actual: hamburgers
+
+console.log("hamburgers" - "s")
+// Prediction: will result in error
+// Actual: NaN
+
+console.log("1" + "3")
+// Prediction: 13
+// Actual: 13
+
+console.log("1" - "3")
+// Prediction: -2
+// Actual: -2
+
+console.log("johnny" + 5)
+// Prediction: type error
+// Actual: 'johnny5'
+
+console.log("johnny" - 5)
+// Prediction: type error
+// Actual: NaN
+
+console.log(99 * "hello")
+// Prediction: hello back to back 99 times
+// Actual: NaN
+
+console.log(1 != 1)
+// Prediction: false
+// Actual: false
+
+console.log(1 == "1")
+// Prediction: true because the value is same
+// Actual: true
+
+console.log(1 === "1")
+// Prediction: false because they are not absolute same
+// Actual: false
