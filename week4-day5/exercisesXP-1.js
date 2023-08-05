76f56b8efc8ca73326ecdcc33bf152f7573be920
orkhan <orkhan.ibrah@gmail.com>
2023-08-05 22:51:24 +0300

    exercise 1 commit

---
+++ week4-day5/exercisesXP-1.js
@@ -0,0 +1,15 @@
+let div = document.querySelector('div');
+console.log(div);
+
+let ul = document.querySelector('ul');
+ul.children[1].innerHTML = 'Richard';
+
+let li = document.querySelectorAll('li');
+li[3].remove();
+
+li[0].innerHTML = 'Orkhan';
+li[2].innerHTML = 'Orkhan';
+
+
+
+
